cbe53512c1648a264468a2b667ef68deaf7c13cb
FateXRebirth <FateXRebirth@gmail.com>
2017-10-29 11:54:47 +0800

    Feature: select area done

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the SettingsPage page.
@@ -15,7 +17,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  state: string;
+  city: string;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private storage: Storage,
+  ) {
+
+    this.storage.get('location').then( result => {
+      if(result != null) {
+        let location = JSON.parse(result);
+        this.state = location.state;
+        this.city = location.city;
+      } else {
+        this.state = 'TW';
+        this.city = 'Taipei';
+      }
+
+    })
+    
+  }
+
+  saveForm() {
+     let location = {
+       state: this.state,
+       city: this.city,
+     }
+     this.storage.set('location', JSON.stringify(location));
+     this.navCtrl.push(HomePage);
   }
 
   ionViewDidLoad() {
